3c2051baf27c4410764327129dbda39d8a92a954
yat0 <brunoccast.gacc@gmail.com>
2017-12-21 18:19:24 +0000

    add edge distance calc to intersect.rb

--- intersect.rb
+++ intersect.rb
@@ -7,6 +7,7 @@
 require 'json'
 require 'set'
 
+
 # degrees to radians
 def to_rad(deg)
   deg*(Math::PI/180)
@@ -27,95 +28,101 @@ def dist(lat1, lon1, lat2, lon2)
   d.round(2)
 end
 
+
 abort("USAGE: ruby intersect.rb filename.json") if ARGV.empty?
 
 map = JSON.parse(File.open(ARGV[0]).read, {:symbolize_names=>true})
 
+# save all osm nodes
+file_nodes = {}
+map[:elements].each do |elem|
+  next if elem[:type] != "node"
+  file_nodes[elem[:id].to_s] = {:lat => elem[:lat], :lon => elem[:lon], :counter => 0}
+end
+
+# save all valid osm ways
+# count each node appearance
 ways = []
-link_count = {}
 way_reg = /^(primary|secondary|tertiary|unclassified|residential)$/
 
 map[:elements].each do |elem|
-
+  
   next if elem[:type] != "way"                      ||
           !elem.has_key?(:tags)                     ||
           !elem[:tags].has_key?(:highway)           ||
           way_reg.match(elem[:tags][:highway]).nil?
   
-  ways << elem # valid way!
-
-  elem[:nodes].each do |node|
-    link_count[node] = {:counter=>0} if link_count[node].nil?
-    link_count[node][:counter] += 1
-  end
-  
+  ways << elem
+  elem[:nodes].each { |node| file_nodes[node.to_s][:counter] += 1 }
 end
 
 puts ways.length.to_s + " valid ways"
 
-
+# find way intersections, build graph
 nodes = Set.new
 edges = []
 
 ways.each do |way|
 
-  abort("Invalid way #" + way[:id]) if way[:nodes].length == 0
+  abort("Invalid way #" + way[:id].to_s) if way[:nodes].length == 0 # sanity check
 
-  start = 0
-  next if start == way[:nodes].length-1 # one node way
+  ix_start = 0
+  next if ix_start == way[:nodes].length-1 # one node way
+  
+  way[:nodes].each_with_index do |cur_node,ix|
+    next if ix==0 || file_nodes[cur_node.to_s][:counter] < 2 # first or not an intersection node
 
-  way[:nodes].each_with_index do |node,ix|
-    next if ix==0 || link_count[node][:counter] < 2 # first or not an intersection node
-
-    n_start = way[:nodes][start]
-
+    n_start = way[:nodes][ix_start].to_s
+    n_end   = way[:nodes][ix].to_s
+    
     nodes << n_start
-    nodes << node
+    nodes << n_end
 
-    e = n_start.to_s + ";" + node.to_s
+    e = n_start + ";" + n_end
 
     if !way[:tags].has_key?(:name)
-      e += ";UNAMED"
+      e << ";UNAMED"
     else
-      e += ";" + way[:tags][:name].gsub(";",'/')
+      e << ";" + way[:tags][:name].gsub(";",'/')
     end
 
-    e += ";" + way[:tags][:highway] + ";Undirected"
+    e << ";" + way[:tags][:highway]
+    e << ";" + dist(file_nodes[n_start][:lat].to_f, file_nodes[n_start][:lon].to_f, file_nodes[n_end][:lat].to_f, file_nodes[n_end][:lon].to_f).to_s
+    e << ";Undirected"
 
     edges << e
-    start = ix
+    ix_start = ix
   end
 
-  next if start == way[:nodes].length-1
+  next if ix_start == way[:nodes].length-1 # road was fully connected
 
-  n_start = way[:nodes][start]
-  n_end   = way[:nodes][-1]
+  n_start = way[:nodes][ix_start].to_s
+  n_end   = way[:nodes][-1].to_s
 
   nodes << n_start
   nodes << n_end
 
-  e = n_start.to_s + ";" + n_end.to_s
+  e = n_start + ";" + n_end
 
   if !way[:tags].has_key?(:name)
-    e += ";UNAMED"
+    e << ";UNAMED"
   else
-    e += ";" + way[:tags][:name].gsub(";",'/')
+    e << ";" + way[:tags][:name].gsub(";",'/')
   end
 
-  e += ";" + way[:tags][:highway] + ";Undirected"
+  e << ";" + way[:tags][:highway]
+  e << ";" + dist(file_nodes[n_start][:lat], file_nodes[n_start][:lon], file_nodes[n_end][:lat], file_nodes[n_end][:lon]).to_s
+  e << ";Undirected"
   
   edges << e
 end
 
 # write nodes file
 str = "Id;Latitude;Longitude\n"
-map[:elements].each do |elem|
-  next if elem[:type] != "node" || !nodes.include?(elem[:id])
-  str += elem[:id].to_s + ";" + elem[:lat].to_s + ";" + elem[:lon].to_s + "\n"
-end
+nodes.each { |node| str << node << ";" << file_nodes[node][:lat].to_s << ";" << file_nodes[node][:lon].to_s << "\n" }
 File.write("Nodes.csv", str)
 
 # write edges file
-str = "Source;Target;Name;Highway;Type\n"
+str = "Source;Target;Name;Highway;Distance;Type\n"
 edges.each { |e| str += e + "\n" }
 File.write("Edges.csv", str)
